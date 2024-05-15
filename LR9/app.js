96d3beaf4d40243918caac5db1ef3eaa15ebd529
Aees <andrej.pelevan@gmail.com>
2024-05-15 14:18:08 +0300

    LR9 ver. 1.0 (Im better, Im faster, Im stronger, I Do IT )

--- LR9/app.js
+++ LR9/app.js
@@ -1,29 +1,50 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-function CountDec() 
-{
+let counter = 0;
+let counterValueElement = document.getElementById('counterValue'); 
 
-
+function CountDec() {
+    counter--;
     
+    if (counter < 0)
+    {
+        counterValueElement.style.color = 'red';
+    }
+    else if (counter == 0)
+    {
+        counterValueElement.style.color = '#222';
+    }
+    else if (counter > 0)
+    {
+        counterValueElement.style.color = 'green';
+    }
+    updateCounterValue();
 }
 
 
-
-
-function CountRes() 
-{
-
-
-    
+function CountRes() {
+    counter = 0;
+    counterValueElement.style.color = '#222';
+    updateCounterValue();
 }
 
 
+function CountInc() {
+    counter++;
+    if (counter < 0)
+    {
+        counterValueElement.style.color = 'red';
+    }
+    else if (counter == 0)
+    {
+        counterValueElement.style.color = '#222';
+    }
+    else if (counter > 0)
+    {
+        counterValueElement.style.color = 'green';
+    }
+    updateCounterValue();
+}
 
-
-
-
-function CountInc() 
-{
-
-
-    
+function updateCounterValue() {
+    counterValueElement.textContent = counter;
 }
